aff226fb0abaacc7add9be4140260113ac320124
Reynis <reynis.vazquez@gmail..com>
2018-05-22 15:11:44 +0000

    create graph of xpander topo and then create mininet xpander topo out of graph

--- xpander-topo.py
+++ xpander-topo.py
@@ -8,14 +8,17 @@ from mininet.topo import Topo
 
 class XpanderTopo(Topo):
 
-    def __init__(self, n_hosts, n_hosts_per_rack, n_ports_per_switch=3, k_lift=2):
+    def __init__(self, n_hosts=12, n_hosts_per_rack=1, n_ports_per_switch=2, k_lift=2):
 
-        self.n_switches = n_hosts/n_hosts_per_rack;
-        self.n_initial_switches = n_ports_per_switch+1;
-        self.n_lifts = math.log(n_switches/n_initial_switches, k_lift);
+        self.n_hosts_per_rack = n_hosts_per_rack
+        n_switches = int(n_hosts/n_hosts_per_rack);
+        n_initial_switches = n_ports_per_switch+1;
+        n_lifts = int(math.log(n_switches/n_initial_switches, k_lift));
 
+        print "Creating an initial complete graph of", n_initial_switches, "switches"
         G = nx.complete_graph(n_initial_switches)
 
+        print "Running", n_lifts, (str(k_lift)+"-lift"), "on graph"
         for _ in range(n_lifts):
             G = self.lift_graph(G, k_lift)
 
@@ -35,9 +38,9 @@ class XpanderTopo(Topo):
         for u, v in old_edges:
             # create k copies of u and k copies of v
             # for every edge {u, v} in old graph
-            if u not in odes_old_to_new.keys():
+            if u not in nodes_old_to_new.keys():
                 nodes_old_to_new[u] = [(str(u) + str(i)) for i in range(k)]
-            if v not in nodes_old_to_new.keys();    
+            if v not in nodes_old_to_new.keys(): 
                 nodes_old_to_new[v] = [(str(v) + str(i)) for i in range(k)]
             u_copies = nodes_old_to_new[u]
             v_copies = nodes_old_to_new[v]
@@ -51,10 +54,24 @@ class XpanderTopo(Topo):
             new_graph.add_edges_from(
                 (u_copies[i], v_copies[i]) for i in range(k));
 
+        return new_graph
+
     def create_topo_from_graph(self, G):
         """ Creates a mininet topology from given networkX graph
         """
-        pass
+        # add top of rack switch per node in G
+        for n in G.nodes():
+            self.addSwitch(n)
+        
+        # connect all top of rack switches
+        for n1, n2 in G.edges():
+            self.addLink(n1, n2)
+
+        # add hosts for each switch
+        for switch in G.nodes():
+            for h in range(self.n_hosts_per_rack):
+                host = self.addHost("h" + switch + "-" + str(h))
+                self.addLink(host, switch)
 
     def addHosts(self):
         for i in range(0, self.nhost):
@@ -99,4 +116,4 @@ class XpanderTopo(Topo):
                              self.hnodes[self.nport / 2 * i + j])
 
 
-topos = { 'xpander' : (lambda: XpanderTopo(8, 2)) }
+topos = { 'xpander' : (lambda: XpanderTopo()) }
