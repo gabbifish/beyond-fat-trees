ad0a97844b4b3aa435f7f1cb9e608b4ac2dea7d5
Reynis <reynis.vazquez@gmail..com>
2018-06-02 22:48:06 +0000

    add command lines to bash script to generate 11a and 11c

--- simulate_graphs.sh
+++ simulate_graphs.sh
@@ -7,6 +7,13 @@
 sudo python experiment.py ftree ecmp active-servers 10
 python analysis.py 10c
 
+# SIMULATE EXPERIMENTS FOR 11a and 11c
+sudo python experiment.py ftree ecmp lambda 10
+sudo python experiment.py xpander ecmp lambda 10
+sudo python experiment.py xpander hyb lambda 10
+
 # GENERATE GRAPH 11(A)
+python analysis.py 11a
 
 # GENERATE GRAPH 11(C)
+python analysis.py 11c
